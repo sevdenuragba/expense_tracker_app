840c38e3cb4efd943fc8b759b5b7b57b68d07f7b
Sevde Nur <sevdenuragba@gmail.com>
2023-08-11 11:07:30 +0300

    Initial commit

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,9 +1,9 @@
-
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
-class Expenses extends StatefulWidget{
+class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
   @override
@@ -12,35 +12,45 @@ class Expenses extends StatefulWidget{
   }
 }
 
-class _ExpensesState extends State<Expenses>{
-final List<Expense> _reqisteredExpenses = [
-  Expense(title: 'Work', 
-  amount:19.99, 
-  date: DateTime.now(),
-  category:Category.work),
-   Expense(title: 'Leisure', 
-  amount:19.99, 
-  date: DateTime.now(),
-  category:Category.leisure),
-   Expense(title: 'Food', 
-  amount:19.99, 
-  date: DateTime.now(),
-  category:Category.food),
-  Expense(title: 'Flight', 
-  amount:19.99, 
-  date: DateTime.now(),
-  category:Category.travel),
-];
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _reqisteredExpenses = [
+    Expense(
+        title: 'Work',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Leisure',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.leisure),
+    Expense(
+        title: 'Food',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        title: 'Flight',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.travel),
+  ];
+
+  void _openAddExpenseOveray() {
+    showModalBottomSheet(context: context, builder: (cntx) => const NewExpense());
+  }
 
   @override
   Widget build(BuildContext context) {
-  return  Scaffold(
-    body: Column(children:
-    
-      [ Expanded (child: ExpensesList(expenses: _reqisteredExpenses)),
-      const Text('hello'),     
-
-    ],)
-  );
+    return Scaffold(
+        appBar: AppBar(title: const Text('Expense Tracker'), actions: [
+          IconButton(
+              onPressed: _openAddExpenseOveray, icon: const Icon(Icons.add))
+        ]),
+        body: Column(
+          children: [
+            Expanded(child: ExpensesList(expenses: _reqisteredExpenses)),
+          ],
+        ));
   }
 }
---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() {
+    return _NewExpenseState();
+  }
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  final _dateController = TextEditingController();
+
+  void _presentDatePicker(){
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 2, now.month, now.day);
+    final lastDate = DateTime.now();
+    showDatePicker(
+    context: context, 
+    initialDate: now, 
+    firstDate: firstDate,
+    lastDate: lastDate);
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    _dateController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 60,
+            decoration: const InputDecoration(
+              label: Text('Expense Title'),
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    label: Text('Expense Amount'),
+                    suffixText: '₺ ',
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const Text('Selected Date'),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(Icons.calendar_month),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                },
+                child: const Text('Add Expense'),
+              ),
+              Row(
+                children: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Cancel'))
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
